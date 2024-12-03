762106a801e12541e82bdc43211d4cacc2a465b7
Vittorio Distefano <info@vdistefano.studio>
2024-12-03 17:32:42 +0100

    chore: fix test script

--- util/test.sh
+++ util/test.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 ## Test all code in the repostitory using cargo test.
 
-for DIR in */; do
+for DIR in src; do
     DIRNAME=$(basename "$DIR")
     echo "==> $DIRNAME <=="
     (cd $DIR && cargo test)
 done
 
-echo "Format complete."
+echo "Test complete."
