79de3a04094b95da64ff16fb068b847303487c5c
Vittorio Distefano <info@vdistefano.studio>
2024-12-03 17:23:32 +0100

    feat: isolated `summarize` command into subcommand_summarize function

--- src/main.rs
+++ src/main.rs
@@ -35,37 +35,40 @@ async fn main() {
     let args: Opts = Opts::parse();
     match args.subcmd {
         SubCommand::Summarize(summarize_opts) => {
-
-            let markdown_files =
-                extract_markdown_files_recursive(&summarize_opts.markdown_files).unwrap();
-
-            let mut summarized_files: HashMap<String, String> = HashMap::new();
-
-            for (filename, content) in markdown_files.iter() {
-                let summarized_text = summarize_content(content);
-                summarized_files.insert(filename.clone(), summarized_text.await);
-            }
-
-            for (filename, summarized_content) in summarized_files.iter() {
-                println!(
-                    "File: {}\nSummarized Content: {}",
-                    filename, summarized_content
-                );
-            }
-
-            let concatenated_summary: String = summarized_files
-            .values()
-            .cloned()
-            .collect::<Vec<String>>()
-            .join("\n\n");
-
-        // summarize and write to an md file
-        let output_file = "output.md";
-        let output = summarize_content(&concatenated_summary).await;
-
-        // write summary to file
-        std::fs::write(output_file, output).unwrap();
-
+            subcommand_summarize(summarize_opts).await;
         }
     }
+}
+
+async fn subcommand_summarize(summarize_opts: SummarizeOpts) {
+
+    let markdown_files =
+    extract_markdown_files_recursive(&summarize_opts.markdown_files).unwrap();
+
+    let mut summarized_files: HashMap<String, String> = HashMap::new();
+
+    for (filename, content) in markdown_files.iter() {
+        let summarized_text = summarize_content(content);
+        summarized_files.insert(filename.clone(), summarized_text.await);
+    }
+
+    for (filename, summarized_content) in summarized_files.iter() {
+        println!(
+            "File: {}\nSummarized Content: {}",
+            filename, summarized_content
+        );
+    }
+
+    let concatenated_summary: String = summarized_files
+    .values()
+    .cloned()
+    .collect::<Vec<String>>()
+    .join("\n\n");
+
+    // summarize and write to an md file
+    let output_file = "output.md";
+    let output = summarize_content(&concatenated_summary).await;
+
+    // write summary to file
+    std::fs::write(output_file, output).unwrap();
 }
