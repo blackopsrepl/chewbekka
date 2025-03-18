e08b8b2672c66fac664efcbbd6539faee6b74bc2
Vittorio Distefano <info@vdistefano.studio>
2025-03-18 15:13:52 +0100

    feat(debloat): Add debloating functionality
    This commit adds the functionality to debloat markdown files. It includes the following changes:
    
    - Import `dissect_subtlety` and `strip_jargon` functions from the `chewbekka::debloat` module
    - Implement the `subcommand_debloat` function, which dissects subtlety, strips jargon, and summarizes all markdown files
    - Create a new test file, `tests/debloat/job.md`, with sample content for debloating
    
    The debloating process involves dissecting subtlety and stripping jargon from each markdown file. The resulting content is then summarized and written to an output file named "output.md".

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use clap::Parser;
 use std::collections::HashMap;
+
 use std::path::PathBuf;
 use std::sync::Mutex;
 
 use chewbekka::extract::extract_markdown_files_recursive;
 use chewbekka::summarize::summarize_content;
+use chewbekka::debloat::dissect_subtlety;
+use chewbekka::debloat::strip_jargon;
 
 #[derive(Parser)]
 #[command(
@@ -81,12 +84,46 @@ async fn subcommand_summarize(summarize_opts: MarkdownFileOpts) {
 
     // summarize and write to an md file
     let output_file = "output.md";
-    let output = summarize_content(&concatenated_summary).await;
+    let output = &concatenated_summary;
 
     // write summary to file
     std::fs::write(output_file, output).unwrap();
 }
 
 async fn subcommand_debloat(debloat_opts: MarkdownFileOpts) {
-}
+// first dissect subtlety, then strip jargon, then summarize all
+    let markdown_files =
+    extract_markdown_files_recursive(&debloat_opts.markdown_files).unwrap();
 
+    let debloated_files: Mutex<HashMap<String, String>> = Mutex::new(HashMap::new());
+
+    let markdown_files = markdown_files.lock().unwrap();
+    for (filename, content) in markdown_files.iter() {
+        let nojargon_text = strip_jargon(content);
+        let debloated_text = dissect_subtlety(&nojargon_text.await).await;
+        let mut debloated_files: std::sync::MutexGuard<'_, HashMap<String, String>> = debloated_files.lock().unwrap();
+        debloated_files.insert(filename.clone(), debloated_text);
+    }
+
+    let debloated_files = debloated_files.lock().unwrap();
+    for (filename, debloated_content) in debloated_files.iter() {
+        println!(
+            "File: {}\nDebloated Content: {}",
+            filename, debloated_content
+        );
+    }
+
+    let concatenated_debloated: String = debloated_files
+    .values()
+    .cloned()
+    .collect::<Vec<String>>()
+    .join("\n\n");
+
+    // summarize and write to an md file
+    let output_file = "output.md";
+    let output = summarize_content(&concatenated_debloated).await;
+
+    // write summary to file
+    std::fs::write(output_file, output).unwrap();
+}
+                                                                                                                                                                      
